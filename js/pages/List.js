9a50d2da7555ff962f6f68a06c6eaa0b109084d6
ruyuw <140332726+ruyuw@users.noreply.github.com>
2024-08-02 12:19:26 +0500

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -1,10 +1,10 @@
-import { store } from "../main.js";
-import { embed } from "../util.js";
-import { score } from "../score.js";
-import { fetchEditors, fetchList } from "../content.js";
+import { store } from "js/main.js";
+import { embed } from "js/util.js";
+import { score } from "js/score.js";
+import { fetchEditors, fetchList } from "js/content.js";
 
-import Spinner from "../components/Spinner.js";
-import LevelAuthors from "../components/List/LevelAuthors.js";
+import Spinner from "js/components/Spinner.js";
+import LevelAuthors from "js/components/List/LevelAuthors.js";
 
 const roleIconMap = {
     owner: "crown",
@@ -138,7 +138,7 @@ export default {
     }),
     computed: {
         level() {
-            return this.list[this.selected][null];
+            return this.list[this.selected][0];
         },
         video() {
             if (!this.level.showcase) {
