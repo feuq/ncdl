02a66460e96b3b9ad1984a6567b9f6d8ccae6baf
ruyuw <140332726+ruyuw@users.noreply.github.com>
2024-08-05 00:24:17 +0500

    Update content.js

--- js/content.js
+++ js/content.js
@@ -3,7 +3,7 @@ import { round, score } from './score.js';
 /**
  * Path to directory containing `_list.json` and all levels
  */
-const dir = 'https://github.com/ruyuw/ncdl/data';
+const dir = '/ncdl/data';
 
 export async function fetchList() {
     const listResult = await fetch(`${dir}/_list.json`);
