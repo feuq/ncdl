ea7c66ba49049a5d1e56abc25b0e1f60bf5d9fd5
ruyuw <140332726+ruyuw@users.noreply.github.com>
2024-09-06 08:42:48 +0500

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,7 +11,7 @@ const scale = 1;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 58) {
+    if (rank > 222) {
         return 0;
     }
     /*if (rank > 75 && percent < 100) {
