9b110974e94384e862f5d4a150c81fb522ba3d85
ruyuw <140332726+ruyuw@users.noreply.github.com>
2024-09-06 08:31:44 +0500

    Update score.js

--- js/score.js
+++ js/score.js
@@ -17,13 +17,7 @@ export function score(rank, percent, minPercent) {
     /*if (rank > 75 && percent < 100) {
         return 0;
     }*/
-
-    // Old formula
-    /*
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
-    */
-    // New formula
+    
     let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)) * 2.5);
 
