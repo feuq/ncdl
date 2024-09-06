6afc4a7e3676c3c75859b01eb2481793b9ec4755
ruyuw <140332726+ruyuw@users.noreply.github.com>
2024-09-06 08:56:55 +0500

    Update score.js

--- js/score.js
+++ js/score.js
@@ -22,8 +22,8 @@ export function score(rank, percent, minPercent) {
     let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)) * 2.5);
     */
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 50) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)) * 2.5);
+    let score = (-24.9975*Math.pow(rank-1, 0.4) + 100) *
+        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)) * 5);
 
     score = Math.max(0, score);
 
